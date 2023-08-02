b8384a8ffd69ff55a300b0c3abdf5f560d2dc421
An2anetti <141183645+An2anetti@users.noreply.github.com>
2023-08-02 05:44:30 +0300

    Update HighLevelAnalyzer.py
    v1.0.4

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -1,51 +1,48 @@
-from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, StringSettingOption
+from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, StringSetting
 
 class CxpiAnalyzer(HighLevelAnalyzer):
     # Определение настроек анализатора
-    my_string_setting = StringSettingOption(
-        id='my_string_setting',
-        name='My String Setting',
-        description='Enter a string:',
-        default_value='Hello, World!'
-    )
+    my_string_setting = StringSetting(
+    name='My String Setting',
+    description='Enter a string:',
+    default_value='Hello, World!'
+)
 
-    def __init__(self):
+    def __init__(self, settings):
         # Инициализация анализатора
         self.string_setting = self.my_string_setting
+        super().__init__(settings=settings)
 
     def decode(self, frame: AnalyzerFrame):
-        # Получаем данные из текущего фрейма
-        data = frame.data['data']
+        # Реализация метода decode() анализатора
+        # Получение байта данных
+        data = frame.data['data'][0]
 
-        # Проверяем длину пакета
-        if len(data) < 2:
-            # Пакет не полный, пропускаем его
-            return None
+        # Добавление кадра данных в вывод анализатора
+        if len(frame.data['data']) > 1:
+            # Кадр с битом четности
+            parity = frame.data['data'][1]
+            self.add_frame(
+                frame=frame,
+                start=frame.start_time,
+                end=frame.end_time,
+                data=data,
+                parity=parity
+            )
+        else:
+            # Кадр без бита четности
+            self.add_frame(
+                frame=frame,
+                start=frame.start_time,
+                end=frame.end_time,
+                data=data
+            )
 
-        # Проверяем, что первый байт соответствует стартовому байту протокола CXPI
-        if data[0] != 0x80:
-            # Пакет не соответствует протоколу CXPI, пропускаем его
-            return None
+    def get_value_string(self, frame: AnalyzerFrame):
+        # Форматирование вывода анализатора
+        if 'parity' in frame.data:
+            return f'{frame.data["data"]}, P{frame.data["parity"]}'
+        else:
+            return f'{frame.data["data"]}'
 
-        # Извлекаем данные из пакета
-        data_length = data[1]
-        message_id = data[2]
-        message_data = data[3:3+data_length]
-
-        # Создаем объект AnalyzerFrame с результатами анализа
-        result_data = {
-            'data_length': data_length,
-            'message_id': message_id,
-            'message_data': message_data
-        }
-        result_frame = AnalyzerFrame(
-            'CXPI', 
-            frame.start_time, 
-            frame.end_time, 
-            result_data,
-            metadata=None,
-            attachments=None
-        )
-
-        # Возвращаем результат анализа
-        return result_frame
+analyzer = CxpiAnalyzer
