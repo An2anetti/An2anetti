584c5bedb6e11e1f7ad751f10418ccb9655fe20e
An2anetti <141183645+An2anetti@users.noreply.github.com>
2023-10-24 03:25:10 +0300

    Update CxpiAnalyzer.py
    v3.4.0

--- CxpiAnalyzer.py
+++ CxpiAnalyzer.py
@@ -10,32 +10,36 @@ class CxpiAnalyzer(HighLevelAnalyzer):
 
     CXPI_START = 0xC2
     CXPI_END = 0xC3
-    CXPI_BIT_0 = 11.375
-    CXPI_BIT_1 = 21.542
+    THRESHOLD = 0.5  # Пороговое значение для определения "0" и "1" в долях времени
 
     def __init__(self):
         self.current_frame = []
-        self.bit_length = self.CXPI_BIT_0
+        self.bit_length = 0
 
     def decode(self, frame):
         if len(frame) > 0:
             if frame[0] == self.CXPI_START:
                 # Start of new frame
                 self.current_frame = []
-                self.bit_length = self.CXPI_BIT_0
 
             self.current_frame.extend(frame)
 
             if frame[-1] == self.CXPI_END:
                 # End of frame
-                data = ''.join('1' if self.bit_length == self.CXPI_BIT_1 else '0' for _ in range(len(self.current_frame)))
-                return AnalyzerFrame('frame', len(self.current_frame), {'data': data})
+                data = []
+
+                for duration in self.current_frame:
+                    bit = '1' if duration > self.bit_length * self.THRESHOLD else '0'
+                    data.append(bit)
+
+                data_str = ''.join(data)
+                return AnalyzerFrame('frame', len(self.current_frame), {'data': data_str})
 
     def decode_data(self, data):
         frame = []
 
-        for byte in data:
-            frame.append(ord(byte))
+        for duration in data:
+            frame.append(duration)
 
         return self.decode(frame)
 
